8f2ef1994d7c3d16e1c4c1e091c3e0585bd73903
bcassid5 <bcassid5@uwo.ca>
2017-02-04 14:11:12 -0500

    everything saves except date and residency :/

--- FrontEnd/studentsRecords/app/components/add-record.js
+++ FrontEnd/studentsRecords/app/components/add-record.js
@@ -35,14 +35,22 @@ export default Ember.Component.extend({
       else{
         setGen = "/assets/studentsPhotos/female.png";
       }
-
+      console.log(setGen);
       var newRecord = myStore.createRecord('student',{
         number: this.get('number'),
         firstName: this.get('firstName'),
         lastName: this.get('lastName'),
+        gender: this.get('gen'),
+        //DOB: this.get(''),
         photo: setGen,
+        //resInfo: ,
+        regComments: this.get('regComments'),
+        BOA: this.get('BOA'),
+        admissAvg: this.get('admissAvg'),
+        admissComments: this.get('admissComments'),
 
       });
+      console.log(setGen);
       newRecord.save();
     }
   }
